90d060f75371bcc3df2fc5b5c4a7558d26d5928b
kulakostas <92936150+stanislavkulakovskyi@users.noreply.github.com>
2023-07-17 08:19:10 +0300

    video as background

--- src/layouts/MainLayout/Background/Background.jsx
+++ src/layouts/MainLayout/Background/Background.jsx
@@ -2,6 +2,7 @@
 import bgText from '../../../assets/images/bg_illustration.webp';
 import bg from '../../../assets/lottie/bglow5.gif';
 import styles from "./Background.module.scss";
+import bgVideo from '../../../assets/lottie/bgAnimation4k.mp4';
 
 export const Background = ({ activePage }) => {
   const isBgWhite = activePage === 'artists' || activePage === 'services';
@@ -9,7 +10,10 @@ export const Background = ({ activePage }) => {
   return (
     <>
       <div className={styles.container}>
-        <img src={bg} alt="background" className={styles.animation} />
+        {/* <img src={bg} alt="background" className={styles.animation} /> */}
+        <video autoPlay loop muted poster={bg} className={styles.animation}>
+            <source src={bgVideo} type='video/mp4' />
+        </video>
       </div>
 
       <div className={styles.backdrop} style={{ backgroundColor: isBgWhite ? '#f5f1f0' : 'transparent' }}></div>
