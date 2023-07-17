d0c5c2e697cfb83318ff94a8dc9171416b89af7d
kulakostas <92936150+stanislavkulakovskyi@users.noreply.github.com>
2023-07-17 15:36:05 +0300

    some minor fixes n updates

--- src/layouts/MainLayout/Background/Background.jsx
+++ src/layouts/MainLayout/Background/Background.jsx
@@ -1,18 +1,24 @@
 /* eslint-disable react/prop-types */
 import bgText from '../../../assets/images/bg_illustration.webp';
-import bg from '../../../assets/lottie/bglow5.gif';
+// import bg from '../../../assets/lottie/bglow5.gif';
+import poster from '../../../assets/lottie/fallback.webp';
 import styles from "./Background.module.scss";
-import bgVideo from '../../../assets/lottie/bgAnimation4k.mp4';
+import bgVideo from '../../../assets/lottie/bgAnimation1080.mp4';
+import bgVideo4k from '../../../assets/lottie/bgAnimation4k.mp4';
+import bgVideo2k from '../../../assets/lottie/bgAnimation2k.mp4';
+import { useCallback } from 'react';
 
 export const Background = ({ activePage }) => {
   const isBgWhite = activePage === 'artists' || activePage === 'services';
+  const isSmall = window.innerWidth <= 1080;
+  const isMedium = window.innerWidth <= 1440;
  
   return (
     <>
       <div className={styles.container}>
         {/* <img src={bg} alt="background" className={styles.animation} /> */}
-        <video autoPlay loop muted poster={bg} className={styles.animation}>
-            <source src={bgVideo} type='video/mp4' />
+        <video loading="lazy" autoPlay loop muted poster={poster} className={styles.animation}>
+            <source loading="lazy" src={isSmall ? bgVideo : isMedium ? bgVideo2k : bgVideo4k} type='video/mp4' />
         </video>
       </div>
 
