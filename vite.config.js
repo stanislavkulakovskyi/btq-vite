e8e638a4a496b4f33fa98785cf311d639a1a6a56
makspooh <maksimpooh@gmail.com>
2023-10-14 18:03:14 +0300

    define .env in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,33 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import envPlugin from 'vite-plugin-env'
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@styles': path.resolve(__dirname, './src/styles'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@api': path.resolve(__dirname, '../src/api'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@layouts': path.resolve(__dirname, './src/layouts'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-      '@fonts': path.resolve(__dirname, './src/fonts'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
+export default defineConfig(({ mode })=> {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@styles': path.resolve(__dirname, './src/styles'),
+        '@components': path.resolve(__dirname, './src/components'),
+        '@assets': path.resolve(__dirname, './src/assets'),
+        '@api': path.resolve(__dirname, '../src/api'),
+        '@pages': path.resolve(__dirname, './src/pages'),
+        '@layouts': path.resolve(__dirname, './src/layouts'),
+        '@utils': path.resolve(__dirname, './src/utils'),
+        '@fonts': path.resolve(__dirname, './src/fonts'),
+        '@hooks': path.resolve(__dirname, './src/hooks'),
+      },
     },
-  },
-  plugins: [react(), envPlugin()],
+    define: {
+      'process.env': {
+        VITE_CONTACT_SERVICE_KEY: JSON.stringify(process.env.VITE_CONTACT_SERVICE_KEY),
+        VITE_CONTACT_TEMPLATE_KEY: JSON.stringify(process.env.VITE_CONTACT_TEMPLATE_KEY),
+        VITE_CONTACT_SECRET_KEY: JSON.stringify(process.env.VITE_CONTACT_SECRET_KEY),
+      },
+    },
+    plugins: [react()],
+  }
 })
