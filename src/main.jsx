432b562c7e499522f1abf417fd654032b4db0eea
Maksim Pokhiliy <maksim.pokhiliy@gmail.com>
2026-06-05 08:15:54 +0300

    feat: prerender routes via vite-react-ssg, wire build and hosting

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import { ViteReactSSG } from 'vite-react-ssg';
+import { routes } from './App';
 import './styles/index.scss';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+export const createRoot = ViteReactSSG({ routes });
